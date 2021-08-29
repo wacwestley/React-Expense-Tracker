0b8e66ed508e03c2114059958c25cccc11e43af5
WestleyCer <wacwestley@gmail.com>
2021-08-29 17:02:36 -0400

    Base app created with just React. Reducers working and states working.

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
     const { transactions } = useContext(GlobalContext);
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState'
